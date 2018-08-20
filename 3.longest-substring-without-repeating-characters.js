93ac363d1a77b645fcb3ac06d43ffefa1202fe1b
Tank Yu <ytyu1024@gmail.com>
2018-08-21 00:18:51 +0800

    3

---
+++ 3.longest-substring-without-repeating-characters.js
@@ -0,0 +1,65 @@
+/*
+ * [3] Longest Substring Without Repeating Characters
+ *
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (24.89%)
+ * Total Accepted:    550.9K
+ * Total Submissions: 2.2M
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * Given a string, find the length of the longest substring without repeating
+ * characters.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "abcabcbb"
+ * Output: 3 
+ * Explanation: The answer is "abc", which the length is 3.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3. 
+ * ⁠            Note that the answer must be a substring, "pwke" is a
+ * subsequence and not a substring.
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let max = 0
+    let rec = ""
+    for (let i = 0; i < s.length; ++i) {
+        let pos = rec.indexOf(s[i])
+        if (pos === -1 ) {
+            rec += s[i]
+            max = Math.max(max, rec.length)
+        } else {
+            rec = rec.slice(pos + 1) + s[i]
+        }
+    }
+    return max
+};
